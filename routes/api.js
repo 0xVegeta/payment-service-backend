e5978c92e79ce53054fb66d3f2c533fb4cb617d1
Anas <anasejaz24@gmail.com>
2023-04-08 18:43:40 +0530

    Correcting wallet routes

--- routes/api.js
+++ routes/api.js
@@ -2,5 +2,6 @@ const express = require('express');
 const apiRouters = express.Router();
 apiRouters.use('/v1/org', require('./org'));
 apiRouters.use('/v1/txn', require('./txn'));
+apiRouters.use('/v1/wallets', require('./wallets'))
 
 module.exports = {apiRouters};
