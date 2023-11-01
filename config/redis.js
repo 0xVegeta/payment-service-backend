b87ea1365ad3093cf30166b07c731007e9d08a7a
0xVegeta <anasejaz24@gmail.com>
2023-11-01 18:01:05 +0530

    Minor changes in redis config

--- config/redis.js
+++ config/redis.js
@@ -4,8 +4,11 @@ const { promisifyAll } = require('bluebird');
 promisifyAll(redis);
 
 const client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
 });
 
 client.on("connect", () => {
